c6e95c1e9bea567b35f1632df663ad0f10b5e911
Oleg Andreev <oleganza@gmail.com>
2014-08-26 10:21:46 +0200

    more on performance

--- BTCScrypt.h
+++ BTCScrypt.h
@@ -4,8 +4,10 @@
 
 #import <Foundation/Foundation.h>
 
-// These parameters require 3-5 seconds and 64 Mb of memory:
+// These parameters require 3-4 seconds on Intel Core i7 2GHz (23-25 seconds on iPhone 5) and 64 Mb of memory:
 //   N = 1<<16, r = 8, p = 4.
+// These parameters require 1 second on Intel Core i7 2GHz (5-6 seconds on iPhone 5) and 64 Mb of memory:
+//   N = 1<<16, r = 8, p = 1.
 //
 // - N is a general "load" parameter. Increase it to require more memory and make computation slower.
 // - r is a "memory" tuning parameter. Make it higher to require more memory without increasing CPU time much.
